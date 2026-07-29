a3bd49210a161cc232c0cf3df834dafb43ff150f
Pratik Kedari <pratikkedari96@gmail.com>
2026-07-29 22:54:31 +0530

    initialize hotel reservation engine backend

---
+++ backend/src/test/java/com/pratik/hotelreservation/HotelreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.hotelreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
