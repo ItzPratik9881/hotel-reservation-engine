d06024c4d6128632b5cce5a260063287118b5fea
Pratik Kedari <pratikkedari96@gmail.com>
2026-08-13 20:33:08 +0530

    feat: complete day 11 api quality improvements

--- backend/src/main/java/com/pratik/hotelreservation/config/OpenApiConfig.java
+++ backend/src/main/java/com/pratik/hotelreservation/config/OpenApiConfig.java
@@ -1,15 +1,29 @@
 package com.pratik.hotelreservation.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Hotel Reservation Engine API",
+                version = "1.0.0",
+                description = "Enterprise Hotel Reservation Engine REST API"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class OpenApiConfig {
 
     @Bean
@@ -17,18 +31,30 @@ public class OpenApiConfig {
 
         final String securitySchemeName = "bearerAuth";
 
-        return new OpenAPI()
-                .info(new Info()
+        io.swagger.v3.oas.models.info.Info apiInfo =
+                new io.swagger.v3.oas.models.info.Info()
                         .title("Hotel Reservation Engine API")
                         .description(
                                 "REST API for managing hotels, rooms, " +
                                 "users, reservations, payments and analytics."
                         )
                         .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Pratik Kedari")
+                        .contact(
+                                new io.swagger.v3.oas.models.info.Contact()
+                                        .name("Pratik Kedari")
+                        );
+
+        io.swagger.v3.oas.models.security.SecurityScheme jwtScheme =
+                new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(
+                                io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP
                         )
-                )
+                        .scheme("bearer")
+                        .bearerFormat("JWT");
+
+        return new OpenAPI()
+                .info(apiInfo)
                 .addSecurityItem(
                         new SecurityRequirement()
                                 .addList(securitySchemeName)
@@ -37,11 +63,7 @@ public class OpenApiConfig {
                         new Components()
                                 .addSecuritySchemes(
                                         securitySchemeName,
-                                        new SecurityScheme()
-                                                .name(securitySchemeName)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
+                                        jwtScheme
                                 )
                 );
     }
